a2f8b8f09f3710f7db95d4587ab219110b8fc33f
Ha Quach Phu Thanh <91040962+MapleThanh@users.noreply.github.com>
2024-02-15 13:32:10 +0700

    Initial commit

---
+++ app/src/main/java/com/example/portraitarchive/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.portraitarchive
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.portraitarchive.ui.theme.PortraitArchiveTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PortraitArchiveTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Ha Quach Vinh Khang NGU")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PortraitArchiveTheme {
+        Greeting("Android")
+    }
+}
